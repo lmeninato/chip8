25092ca3c88555071f71d99bee9ce0f5e00d6db5
Lorenzo Meninato <lorenzomeninato@gmail.com>
2020-03-04 22:48:34 -0500

    TODO lots of stuff

--- main.c
+++ main.c
@@ -20,6 +20,10 @@ typedef struct chip8 {
     unsigned char sound_timer;
 } chip8;
 
+void initialize_chip8(int argc, char **argv);
+void handle_input(int argc, char **argv);
+void write_fontset_to_memory();
+
 chip8 chip8_emu;
 int main(int argc, char **argv){
     if (argc != 2){
@@ -31,7 +35,7 @@ int main(int argc, char **argv){
         exit(1);
     }
 
-    //Get the file size and read it into a memory buffer
+    // Get the file size and read it into a memory buffer
     fseek(f, 0L, SEEK_END);
     int fsize = ftell(f);
     fseek(f, 0L, SEEK_SET);
@@ -39,8 +43,47 @@ int main(int argc, char **argv){
     unsigned char *buffer=malloc(fsize);
 
     fread(buffer, fsize, 2, f);
+    // Write buffer to 0x200 in memory
     fclose(f);
-    
+
     free(buffer);
     return 0;
+}
+
+void initialize_chip8(int argc, char **argv){
+    // chip8_emu is global object
+    handle_input(argc, argv);
+    write_fontset_to_memory();
+}
+
+void handle_input(int argc, char **argv){
+    // parse the input
+
+    // write ROM data to 0x200-0xFFF
+}
+
+void write_fontset_to_memory(){
+    // writes fontset to the following memory addresses: 0x050-0x0A0
+    unsigned char chip8_fontset[80] = {
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    };
+    int i;
+    for (i = 0; i < 80; ++i){
+        chip8_emu.memory[i+0x050] = chip8_fontset[i];
+    }
 }
